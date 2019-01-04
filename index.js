e2a7aa767a2432b78204897ccbe2c14d400d4209
Sojisoyoye <soji.soyoye@gmail.com>
2019-01-04 07:25:17 +0100

    set up app and server entry point

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.status(200).send({ message: 'Welcome to delivery!!!' }));
+
+app.listen(3000);
+console.log('app running on port ', 3000);
