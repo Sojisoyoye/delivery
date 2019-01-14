e17c0f89e98231f4e962100de23f964f1cc67eff
Sojisoyoye <soji.soyoye@gmail.com>
2019-01-14 23:12:43 +0100

    Set up test and modified app

--- routes/packageroutes.js
+++ routes/packageroutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import packageController from '../controllers/packagecontrollers';
+import PackageController from '../controllers/packagecontrollers';
 
 const router = express.Router();
 
-router.post('/api/v1/pckgs', packageController.create);
-router.get('/api/v1/pckgs', packageController.getAll);
-router.get('/api/v1/pckgs/:id', packageController.getOne);
-router.put('/api/v1/pckgs/:id', packageController.update);
-router.delete('/api/v1/pckgs/:id', packageController.delete);
+router.post('/api/v1/pckgs', PackageController.create);
+router.get('/api/v1/pckgs', PackageController.getAll);
+router.get('/api/v1/pckgs/:id', PackageController.getOne);
+router.delete('/api/v1/pckgs/:id', PackageController.delete);
+// router.put('/api/v1/pckgs/:id', packageController.update);
 
 export default router;
