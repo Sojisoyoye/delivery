5416f57ff973953b6193a1cbdb399a61efdf332f
Sojisoyoye <soji.soyoye@gmail.com>
2019-01-15 11:03:08 +0100

    Added a Procfile

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import router from './routes/packageroutes';
 
-// const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -13,14 +13,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(router);
 
+app.get('/', (req, res) => res.status(200).send({ message: 'OK' }));
+
 app.get('/api/v1', (req, res) => res.status(200).send({ message: 'Welcome to delivery!!!' }));
 
 // if (process.env.NODE_ENV !== 'test') {
 // app.listen(port);
 // }
 
-app.listen(3000);
+app.listen(port);
 // eslint-disable-next-line no-console
-console.log('app running on port ', 3000);
+console.log('app running on port ', port);
 
 export default app;
